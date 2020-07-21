69dfd7c97cfeeb50eb7c1ab1b61777d55da1494f
VincentWoglo <vincentwoglo@gmail.com>
2020-07-21 02:51:23 -0600

    My Portfolio V1

--- src/projects.js
+++ src/projects.js
@@ -3,15 +3,36 @@ import './css/style.css';
 
 function Projects(){
     return(
-        <div>
-            <section>
+        <div className="projects">
+            <section className="Card-section-background">
                 <div className="Card-field">
                     <div className="Card">
                         <div className="Card-img"></div>
-                        <h1 className="Card-title"></h1>
-                    </div>
+                        <div className="Card-info">
+                            <h1 className="Card-title">Botsociety landing page</h1>
+                        </div>
+                        </div>
+                </div>
+
+                <div className="Card-field">
+                    <div className="Card">
+                        <div className="Card-img"></div>
+                        <div className="Card-info">
+                            <h1 className="Card-title">Botsociety landing page</h1>
+                        </div>
+                        </div>
+                </div>
+
+                <div className="Card-field">
+                    <div className="Card">
+                        <div className="Card-img"></div>
+                        <div className="Card-info">
+                            <h1 className="Card-title">Botsociety landing page</h1>
+                        </div>
+                        </div>
                 </div>
             </section>
+            <div className="portal"></div>
         </div>
     )
 }
