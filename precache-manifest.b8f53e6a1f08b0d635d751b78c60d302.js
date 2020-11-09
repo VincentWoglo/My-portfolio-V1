a8bfdadb190d4677e21c65e75485faa12ad04a43
VincentWoglo <vincentwoglo@gmail.com>
2020-11-09 09:30:28 -0700

    Updates

---
+++ precache-manifest.b8f53e6a1f08b0d635d751b78c60d302.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f98fe8ee1f677af500e122f769d3b76",
+    "url": "/My-portfolio-V1.github.io/index.html"
+  },
+  {
+    "revision": "3171fc86e4780cd654b8",
+    "url": "/My-portfolio-V1.github.io/static/css/main.36702826.chunk.css"
+  },
+  {
+    "revision": "f9c6b31d0a434f3c491e",
+    "url": "/My-portfolio-V1.github.io/static/js/2.892a6233.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-portfolio-V1.github.io/static/js/2.892a6233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3171fc86e4780cd654b8",
+    "url": "/My-portfolio-V1.github.io/static/js/main.469f9ceb.chunk.js"
+  },
+  {
+    "revision": "0e3fc3b55d55921d0ad7",
+    "url": "/My-portfolio-V1.github.io/static/js/runtime-main.ef52a3de.js"
+  },
+  {
+    "revision": "bddecf6565c91ce12323438a987cdeb0",
+    "url": "/My-portfolio-V1.github.io/static/media/Volo-digital-agency.bddecf65.png"
+  },
+  {
+    "revision": "de699bb2a3969ff04ec850891b8cb9d8",
+    "url": "/My-portfolio-V1.github.io/static/media/care.de699bb2.png"
+  },
+  {
+    "revision": "90130631abfbb6772cc9a7c3075bfad8",
+    "url": "/My-portfolio-V1.github.io/static/media/crossway.90130631.png"
+  },
+  {
+    "revision": "6971ddc761cc35ba918be0a69dfbcf50",
+    "url": "/My-portfolio-V1.github.io/static/media/css-5.6971ddc7.svg"
+  },
+  {
+    "revision": "4a0f438137b9b4632fdf2051fa4efc6c",
+    "url": "/My-portfolio-V1.github.io/static/media/diesoy.4a0f4381.png"
+  },
+  {
+    "revision": "60dca683c4e74cab61a235938c99296b",
+    "url": "/My-portfolio-V1.github.io/static/media/doorchef website.60dca683.png"
+  },
+  {
+    "revision": "d8ffa1bb47986298869642719da6f0fe",
+    "url": "/My-portfolio-V1.github.io/static/media/doorchef.d8ffa1bb.png"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/My-portfolio-V1.github.io/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "540a3ddee6652a69e9da97ea67ceb122",
+    "url": "/My-portfolio-V1.github.io/static/media/iconmonstr-x-mark-thin.540a3dde.svg"
+  },
+  {
+    "revision": "413afd8e1a5ad5572fd94ad6de77570f",
+    "url": "/My-portfolio-V1.github.io/static/media/jquery-2.413afd8e.svg"
+  },
+  {
+    "revision": "7634d1a3c06643e9394adc4ba35d7d2e",
+    "url": "/My-portfolio-V1.github.io/static/media/php-1.7634d1a3.svg"
+  },
+  {
+    "revision": "c3fbd71b12b726e00eec2b59f3c76786",
+    "url": "/My-portfolio-V1.github.io/static/media/react-2.c3fbd71b.svg"
+  },
+  {
+    "revision": "794f112f248efe9cdaf4947b1f9c8785",
+    "url": "/My-portfolio-V1.github.io/static/media/tim-van-der-kuip-CPs2X8JYmS8-unsplash.794f112f.jpg"
+  },
+  {
+    "revision": "adf2de5e5c098d46fc55682ffca55262",
+    "url": "/My-portfolio-V1.github.io/static/media/walmart.adf2de5e.png"
+  },
+  {
+    "revision": "5da470c84992047114fde394842fb3ac",
+    "url": "/My-portfolio-V1.github.io/static/media/wayfair.5da470c8.png"
+  },
+  {
+    "revision": "ef4e3f4d1156b4129676bce2cab47d77",
+    "url": "/My-portfolio-V1.github.io/static/media/zillow.ef4e3f4d.png"
+  }
+]);
